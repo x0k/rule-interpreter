ebab0c5f0856b7cc694971ef93cbc7326c5c77cb
Krasilnikov Roman <x0kill@ya.ru>
2019-05-12 21:36:28 +0300

    Add eval action test

--- tests/actions.js
+++ tests/actions.js
@@ -5,6 +5,9 @@ import { buildActionsBuilder } from '../build'
 const operations = {
   inc (step, value) {
     return value + step
+  },
+  sum (...numbers) {
+    return numbers.reduce((sum, value) => sum + value, 0)
   }
 }
 
@@ -18,4 +21,10 @@ test('Map action', t => {
   const expression = [ '$map', [1, 2, 3], '@inc', [3] ]
   const action = buildAction(expression)
   t.deepEqual(action, [4, 5, 6])
+})
+
+test('Eval action', t => {
+  const expression = [ '$eval', '@sum', [1, 2, 3] ]
+  const action = buildAction(expression)
+  t.is(action, 6)
 })
